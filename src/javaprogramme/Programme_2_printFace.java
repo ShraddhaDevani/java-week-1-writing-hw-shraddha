5cac4dcfd6ee4a447152f1c41e13a9b611929226
ShraddhaDevani <shraddhamorad@gmail.com>
2023-10-20 10:42:58 +0100

    submit

---
+++ src/javaprogramme/Programme_2_printFace.java
@@ -0,0 +1,14 @@
+package javaprogramme;
+
+/**
+ * 2. Write a Java program to print a face.
+ */
+public class Programme_2_printFace {
+    public static void main(String[] args) {
+        System.out.println(" +\"\"\"\"\"+");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  |  ");
+        System.out.println(" | '_' | ");
+        System.out.println(" | '_' | ");
+    }
+}
